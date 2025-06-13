3bdd2b5173fe5fb243eefe373ca53df16c8b4da5
Lauren Huet <lauren.huet94@gmail.com>
2025-06-13 11:06:29 +0800

    updated backup scripts

--- scripts/backup_scripts/backup-loop.sh
+++ scripts/backup_scripts/backup-loop.sh
@@ -1,8 +1,10 @@
-csv_file="backup.csv"
+scripts=/scratch/pawsey0964/lhuet/refgenomes/OceanOmics-OceanGenomes-post-curation/scripts/backup_scripts
+
+csv_file="$scripts/backup.csv"
 
 # Loop through each line of the CSV
 tail -n +2 "$csv_file" | while IFS=',' read -r OG date ver; do
     # Submit the backup script, passing OG and asm_version as arguments
-    sbatch backup.sh "$OG" "$date" "$ver"
+    sbatch $scripts/backup.sh "$OG" "$date" "$ver"
     echo $OG $date $ver
 done
--- scripts/backup_scripts/backup.sh
+++ scripts/backup_scripts/backup.sh
@@ -15,14 +15,8 @@ ver=$3
 asm_ver=${OG}_${date}.${ver}
 sample=$OG
 
-# Input validation
-if [[ -z "$OG" ]]; then
-  echo "Error: OG variable is empty or not set. Exiting."
-  exit 1
-fi
-
-if [[ ! -d $OG ]]; then
-  echo "Error: Directory $OG does not exist. Exiting."
+if [[ -z "$OG" || ! -d $OG ]]; then
+  echo "Error: Missing or invalid input directory: $OG"
   exit 1
 fi
 
@@ -38,10 +32,10 @@ rclone copy "${OG}/merqury/" "pawsey0964:oceanomics-refassemblies/${sample}/${as
 
 # Curated assemblies
 cp "${OG}/update_mapping/${asm_ver}.hap2.chr_level_new.fa" "${OG}/update_mapping/${asm_ver}.3.curated.hap2.chr_level.fa"
-cp "${OG}/Hap_1/${asm_ver}_hap1.chr_level.fa" "${OG}/Hap_1/${asm_ver}.3.curated.hap1.chr_level.fa"
+cp "${OG}/rapid-curation/Hap_1/${asm_ver}_hap1.chr_level.fa" "${OG}/rapid-curation/Hap_1/${asm_ver}.3.curated.hap1.chr_level.fa"
 
 rclone copy "${OG}/update_mapping/${asm_ver}.3.curated.hap2.chr_level.fa" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/assembly" --checksum --progress
-rclone copy "${OG}/Hap_1/${asm_ver}.3.curated.hap1.chr_level.fa" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/assembly" --checksum --progress
+rclone copy "${OG}/rapid-curation/Hap_1/${asm_ver}.3.curated.hap1.chr_level.fa" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/assembly" --checksum --progress
 
 # AGP files
 cp "${OG}/rapid-curation/Hap_1/hap.agp" "${OG}/rapid-curation/Hap_1/${asm_ver}.3.curated.hap1.agp"
@@ -50,21 +44,19 @@ cp "${OG}/rapid-curation/Hap_2/hap.agp" "${OG}/rapid-curation/Hap_2/${asm_ver}.3
 rclone copy "${OG}/rapid-curation/Hap_1/${asm_ver}.3.curated.hap1.agp" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/agp" --checksum --progress
 rclone copy "${OG}/rapid-curation/Hap_2/${asm_ver}.3.curated.hap2.agp" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/agp" --checksum --progress
 
-# BAM files
-rclone copy "${OG}/omnic" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/bam" --checksum --progress
+# BAMs
+rclone copy "${OG}/omnic_hap1" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/bam/omnic_hap1" --checksum --progress
+rclone copy "${OG}/omnic_hap2" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/bam/omnic_hap2" --checksum --progress
 
 # Pretext maps
-rclone copy "${OG}/pretextsnapshot/${asm_ver}.3.curated.hap1.pretext_snapshotFullMap.png" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/pretext" --checksum --progress
-rclone copy "${OG}/pretextsnapshot/${asm_ver}.3.curated.hap2.pretext_snapshotFullMap.png" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/pretext" --checksum --progress
-
-cp "${OG}/${asm_ver}.3.curated..hap1.map.pretext" "${OG}/${asm_ver}.3.curated.hap1.map.pretext"
-
-rclone copy "${OG}/${asm_ver}.3.curated.hap1.map.pretext" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/pretext" --checksum --progress
-rclone copy "${OG}/${asm_ver}.3.curated.hap2.map.pretext" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/pretext" --checksum --progress
+rclone copy "${OG}/pretextmap_hap_1" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/pretext/hap1" --checksum --progress
+rclone copy "${OG}/pretextmap_hap_2" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/pretext/hap2" --checksum --progress
+rclone copy "${OG}/pretextsnapshot_hap1" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/pretext_snapshots/hap1" --checksum --progress
+rclone copy "${OG}/pretextsnapshot_hap2" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/pretext_snapshots/hap2" --checksum --progress
 
 # Stats
-rclone copy "${OG}/${asm_ver}.percentage_stats_output.txt" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/stats" --checksum --progress
-rclone copy "${OG}/${asm_ver}.stats_output.txt" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/stats" --checksum --progress
+rclone copy "${OG}/calculate_stats/${asm_ver}.stats_output.txt" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/stats" --checksum --progress
+rclone copy "${OG}/calculate_stats/percentage_stats_output.txt" "pawsey0964:oceanomics-refassemblies/${sample}/${asm_ver}/stats" --checksum --progress
 
-# MultiQC
+# MultiQC (assuming fixed location)
 rclone copy multiqc "pawsey0964:oceanomics-refassemblies/postcuration_multiqc" --checksum --progress
